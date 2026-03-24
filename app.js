bebcebed975223f897f5ddd6d6c9a403c6c771c9
Dilshad <dhilshadcp88@gmail.com>
2026-03-25 05:14:42 +0530

    test auto pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-res.send("Hello from Kubernetes CI/CD pipeline");
+res.send("Updated version from CI/CD pipeline 🚀");
 });
 
 app.listen(3000, ()=>{
