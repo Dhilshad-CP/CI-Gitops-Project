5e2b08c0fb471d12d6e53e8f6a08bda794a71ba0
Dilshad <dhilshadcp88@gmail.com>
2026-03-25 05:51:40 +0530

    test auto pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-res.send("Updated versions from CI/CD pipeline 🚀");
+res.send("Update versions from CI/CD pipeline 🚀");
 });
 
 app.listen(3000, ()=>{
